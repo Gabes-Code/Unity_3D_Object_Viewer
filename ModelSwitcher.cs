43103a7b7649819f12a9646422ee93752804be1c
Gabriel Huseas <gabescode@protonmail.com>
2025-07-01 19:22:23 -0600

    Add files via upload

---
+++ ModelSwitcher.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class ModelSwitcher : MonoBehaviour
+{
+    public GameObject[] models; // Assign your models in the Inspector
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        ShowOnlyCurrentModel();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            SwitchModel(-1);
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            SwitchModel(1);
+    }
+
+    void SwitchModel(int direction)
+    {
+        currentIndex += direction;
+
+        if (currentIndex < 0)
+            currentIndex = models.Length - 1;
+        else if (currentIndex >= models.Length)
+            currentIndex = 0;
+
+        ShowOnlyCurrentModel();
+    }
+
+    void ShowOnlyCurrentModel()
+    {
+        for (int i = 0; i < models.Length; i++)
+        {
+            models[i].SetActive(i == currentIndex);
+        }
+    }
+}

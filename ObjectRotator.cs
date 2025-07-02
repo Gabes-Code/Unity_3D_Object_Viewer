0c600511b0fc29b3837134885f2969f8be699026
Gabriel Huseas <gabescode@protonmail.com>
2025-07-01 19:22:37 -0600

    Add files via upload

---
+++ ObjectRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float rotX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            float rotY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up, -rotX, Space.World);
+            transform.Rotate(Vector3.right, rotY, Space.World);
+        }
+    }
+}
